2c1c69caa8d1828a964902f84749dfb27874ebb8
Mahmoud Mousa <Mahmoud.m@covergenius.com>
2022-08-30 16:05:08 +1000

    Init try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Will this work?</h1>
       </header>
+      <iframe src="https://codesandbox.io/embed/sandbox-test-o1b9ck?fontsize=14&hidenavigation=1&theme=dark"
+     style={{  width: "100%",
+     height: "500px",
+     border: "0",
+     borderRadius: "4px",
+     overflow: "hidden"}}
+     title="sandbox-test"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+   ></iframe>
     </div>
   );
 }
